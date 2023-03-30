c0da4cf0f7d37d3e160c91dea47f4ac92db2b885
Mohammadpatel176 <mohammadpatel7402@gmail.com>
2023-03-30 15:31:25 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,141 @@
+# Import dependencies
+from flask import Flask, render_template, request, jsonify
+import joblib
+import pandas as pd
+import pickle as pkl
+
+# Initialize Flask app
+app = Flask(__name__)
+
+# Load ML model and data scaler
+model = joblib.load('static/data/Resources/model.joblib')
+scaler = joblib.load('static/data/Resources/scaler.joblib')
+
+
+# Home Page
+@ app.route('/')
+def home():
+    return render_template('home.html')
+
+
+# Predictor Page
+@ app.route('/predictor')
+def predictor():
+    return render_template('predictor.html')
+
+# Prediction API fetch
+@ app.route('/predict', methods=['POST'])
+def predict():
+
+    dataInput = (request.json)
+    data = {}
+    try:
+
+        for x in range(52):
+            if int(dataInput['week']) == x+1:
+                data[f"week_of_year_{x+1}"] = [1]
+            else:
+                data[f"week_of_year_{x+1}"] = [0]
+        for x in range(10):
+            if int(dataInput['store']) == x+1 and x+1 != 9:
+                data[f"Store_{x+1}"] = [1]
+            elif x+1 != 9:
+                data[f"Store_{x+1}"] = [0]
+            else:
+                continue
+        for x in range(3):
+            if int(dataInput['product']) == x+1:
+                data[f"Product_{x+1}"] = [1]
+            else:
+                data[f"Product_{x+1}"] = [0]
+        if float(dataInput["price"]) < float(dataInput["basePrice"]):
+            data['promotion'] = [1]
+        else:
+            data['promotion'] = [0]
+        data["Base Price"] = [float(dataInput["basePrice"])]
+        data["Price"] = [float(dataInput["price"])]
+        data["Is_Holiday"] = [float(dataInput["holiday"])]
+
+
+    except:
+        return jsonify({'quantity': -1})
+
+    input_df = pd.DataFrame(data)
+    input_scaled = scaler.transform(input_df)
+    output = model.predict(input_scaled)
+
+    return jsonify({"quantity": output[0]})
+
+
+@ app.route('/projections')
+def projections():
+    return render_template('projections.html')
+
+
+#
+@ app.route('/predict_projections', methods=['POST'])
+def predict_projections():
+
+    dataInput = (request.json)
+    data = {}
+    try:
+
+        for x in range(52):
+            if int(dataInput['week']) == x+1:
+                data[f"week_of_year_{x+1}"] = [1]
+            else:
+                data[f"week_of_year_{x+1}"] = [0]
+        for x in range(10):
+            if int(dataInput['store']) == x+1 and x+1 != 9:
+                data[f"Store_{x+1}"] = [1]
+            elif x+1 != 9:
+                data[f"Store_{x+1}"] = [0]
+            else:
+                continue
+        for x in range(3):
+            if int(dataInput['product']) == x+1:
+                data[f"Product_{x+1}"] = [1]
+            else:
+                data[f"Product_{x+1}"] = [0]
+        if int(dataInput["price"]) < int(dataInput["basePrice"]):
+            data['promotion'] = [1]
+        else:
+            data['promotion'] = [0]
+        data["Base Price"] = [int(dataInput["basePrice"])]
+        data["Price"] = [int(dataInput["price"])]
+        data["Is_Holiday"] = [int(dataInput["holiday"])]
+
+    except:
+        return jsonify({'quantity': -1})
+
+    input_df = pd.DataFrame(data)
+    output = model.predict(input_df)
+
+    return jsonify({"quantity": output[0]})
+
+
+# Data Page
+@app.route('/data')
+def data():
+    return render_template('data.html')
+
+
+# About Page
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+# Error Handler Page
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
+
+
+if __name__ == '__main__':
+
+    # Run this when running on LOCAL server...
+    app.run(debug=True)
+
+    # ...OR run this when PRODUCTION server.
+    # app.run(debug=False)
---
+++ static/js/predictor.js
@@ -0,0 +1,106 @@
+console.log('predictor page')
+
+
+// CREATE DROPDOWN MENUS
+
+// Store
+var dropdown = document.getElementById("selStore1");
+var opt = document.createElement("option");
+opt.text = 'Select Store';
+opt.value = '';
+dropdown.add(opt);
+
+for (var element = 1; element < 11; element++) {
+    var dropdown = document.getElementById("selStore1");
+    var opt = document.createElement("option");
+    let str = "Store ";
+    str += element.toString();
+    opt.text = str.toString();
+    opt.value = element;
+    dropdown.add(opt);
+}
+
+// Product
+var dropdown = document.getElementById("selProduct1");
+var opt = document.createElement("option");
+opt.text = 'Select Product';
+opt.value = '';
+dropdown.add(opt);
+
+for (var element = 1; element < 4; element++) {
+    var dropdown = document.getElementById("selProduct1");
+    var opt = document.createElement("option");
+    let str = "Product ";
+    str += element.toString();
+    opt.text = str.toString();
+    opt.value = element;
+    dropdown.add(opt);
+}
+
+// Week of the Year
+var dropdown = document.getElementById("selWeek1");
+var opt = document.createElement("option");
+opt.text = 'Select Week';
+opt.value = '';
+dropdown.add(opt);
+
+var unique = [...Array(52).keys()];
+unique.forEach((element) => {
+    var dropdown = document.getElementById("selWeek1");
+    var opt = document.createElement("option");
+    let str = "Week ";
+    str += element + 1;
+    opt.text = str.toString();
+    opt.value = element + 1;
+    dropdown.add(opt);
+}
+);
+
+
+// BUTTON EVENT HANDLING
+
+d3.select("button.p1").on("click", function () {
+
+    var store = d3.select("#selStore1").property("value");
+    var product = d3.select("#selProduct1").property("value");
+    var week = d3.select("#selWeek1").property("value");
+    var holiday = d3.select("#selHoliday1").property("value");
+    var basePrice = d3.select("#basePrice1").property("value");
+    var price = d3.select("#price1").property("value");
+    var cost = d3.select("#cost1").property("value");
+
+    data = {
+        "store": store,
+        "product": product,
+        "week": week,
+        "holiday": holiday,
+        "basePrice": basePrice,
+        "price": price,
+        "cost": cost,
+    };
+
+    console.log(data);
+
+    fetch('/predict', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+        .then(response => response.json())
+        .then(d => {
+            console.log('response', d)
+            var quantity = d.quantity
+            var revenue = quantity * price;
+            var costT = quantity * cost;
+            var profit = revenue - costT;
+
+            document.getElementById("modelResults1").innerHTML = "<br>Quantity Sold:   <strong>" + quantity.toFixed(0) + "</strong>" +
+                "<br><br>Total Revenue:   <strong>$" + revenue.toFixed(2) + "</strong>" +
+                "<br><br>Total Cost:   <strong>$" + costT.toFixed(2) + "</strong>" +
+                "<br><br>Total Profit:   <strong>$" + profit.toFixed(2) + "</strong>";
+        }
+        )
+}
+);
